8661077b756119f00efb36e42ebf34180db6fbd6
Cinco <keneth.cinco@dxc.com>
2025-07-15 15:40:22 +0800

    Commit SWA Prod

--- OneCap/src/app/core/util/validator.util.ts
+++
@@ -1,9 +0,0 @@
-import { ValidatorFn } from '@angular/forms';
-
-export const isNotString: ValidatorFn = (control) => {
-  return (typeof control.value === 'string' && control.value.trim() !== '') ? {
-    valueIsString: {
-      value: control.value
-    }
-  } : null;
-};
--- OneCap/src/constant/app.constant.ts
+++
@@ -1,8 +0,0 @@
-
-export enum ContentType {
-  ALL_TYPE = 'All Types',
-  LEARNING_MAPS = 'Learning Maps',
-  CURRICULA = 'Curricula',
-  COURSES = 'Courses',
-  EXAMS = 'Exams',
-}
